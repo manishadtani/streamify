18b68096b754a4baf17ccff595b268c98b9ac250
manish-adtani1 <manishadtani99@gmail.com>
2025-05-17 20:52:32 +0530

    fix: expand allowedHeaders in CORS configuration for enhanced API request support

--- backend/src/app.js
+++ backend/src/app.js
@@ -12,7 +12,7 @@ app.use(cors({
     origin: ['https://streamify-murex.vercel.app','http://localhost:5173'],
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE',"OPTIONS"],
-    allowedHeaders: ['Content-Type', 'Authorization'],
+    allowedHeaders: ['Content-Type', 'Authorization', 'Accept', 'X-Requested-With', 'Origin',"X-Access-Token"],
 }));
 
 
