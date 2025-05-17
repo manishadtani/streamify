f62b402245dd0a6ac11ea79277907b0fbde59177
manish-adtani1 <manishadtani99@gmail.com>
2025-05-17 21:01:53 +0530

    fix: update SameSite attribute in loginController cookie to 'none' for cross-site requests

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -112,7 +112,7 @@ export const loginController = async (req, res) => {
         res.cookie("jwt", token, {
             maxAge: 7 * 24 * 60 * 1000,
             httpOnly: true,
-            sameSite: "strict",
+            sameSite: "none",
             secure: process.env.NODE_ENV === "production"
         })
 
