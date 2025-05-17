b9f97744675c673f11b441aec6a2fa17bf820341
manish-adtani1 <manishadtani99@gmail.com>
2025-05-17 18:25:56 +0530

    fix: add package.json and package-lock.json for bcrypt and cors dependencies

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -4,6 +4,4 @@ import axios from 'axios'
 export const axiosInstance = axios.create({
     baseURL: "https://streamify-ua7t.onrender.com/api",
     withCredentials:true,  // send cookiew with the request
-    
-
 })
